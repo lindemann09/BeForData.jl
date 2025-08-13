50d9428c0db3fd8929452e44475d5fd7d95fe2ff
Oliver Lindemann <lindemann09@googlemail.com>
2025-08-13 17:31:21 +0200

    filtering as extension

--- src/BeForData.jl
+++ src/BeForData.jl
@@ -2,37 +2,40 @@ module BeForData
 
 using Statistics: mean
 using DataFrames
-using DSP
 
 export BeForRecord,
-        forces,
-        time_stamps,
-        split_sessions,
-        session_range,
-        add_session,
-        find_samples_by_time
+	forces,
+	time_stamps,
+	split_sessions,
+	session_range,
+	add_session,
+	find_samples_by_time
 
 export BeForEpochs,
-        vcat,
-        extract_epochs,
-        adjust_baseline!
+	vcat,
+	extract_epochs,
+	adjust_baseline!
 
 export scale_force!,
-        lowpass_filter,
-        filtfilt_record,
-        moving_average,
-        detrend,
-        detect_sessions
+	detect_sessions,
+	moving_average,
+	detrend
 
 include("record.jl")
 include("epochs.jl")
 include("preprocessing.jl")
 
+
+## Filtering support (requires DSP)
+function filtfilt_record end
+function lowpass_filter end
+export filtfilt_record, lowpass_filter
+
 ## Arrow support (requires Arrow)
 function write_feather end
 export write_feather
 
-## XDF support (requires XDF and EzXML)
+## XDF support (requires XDF)
 function xdf_data end
 function xdf_channel_info end
 export xdf_data, xdf_channel_info
--- src/preprocessing.jl
+++ src/preprocessing.jl
@@ -16,63 +16,6 @@ function scale_force!(fd::BeForEpochs, factor::Real)
 	return fd
 end
 
-## FILTER using DSP
-function filtfilt_record(coef::FilterCoefficients, rec::BeForRecord)::BeForRecord
-
-	rtn = copy(rec)
-	for r in session_range(rtn)
-		for c in rtn.force_cols
-			rtn.dat[r, c] = filtfilt(coef, rtn.dat[r, c])
-		end
-	end
-	return rtn
-end
-
-function filtfilt_record(b::AbstractVector, a::AbstractVector, rec::BeForRecord)::BeForRecord
-
-	rtn = copy(rec)
-	for r in session_range(rtn)
-		for c in rtn.force_cols
-			rtn.dat[r, c] = filtfilt(b, a, rtn.dat[r, c])
-		end
-	end
-	return rtn
-end
-
-function lowpass_filter(rec::BeForRecord;
-	cutoff::Real,
-	order::Integer)
-
-	butter_flt = digitalfilter(Lowpass(cutoff), Butterworth(order);
-		fs = rec.sampling_rate)
-	rtn = filtfilt_record(butter_flt, rec)
-
-	rtn.meta["filter"] = "butterworth: cutoff="*string(cutoff)*", order="*string(order)
-	return rtn
-end
-
-function lowpass_filter(fe::BeForEpochs;
-	cutoff::Real,
-	order::Integer = 4,
-	center_data::Bool = true,
-	suppress_warning::Bool = false,
-) # FIXME old method, maybe remove center_data
-	suppress_warning || @warn "It's suggested to filter the data (i.e. BeForRecord) " *
-							  "before creating epochs, because each epoch will be filter individually. " *
-							  "This might causes artifacts"
-
-	rtn = copy(fe)
-	sampling_rate = rtn.sampling_rate
-	for row in eachrow(rtn.dat)
-		row[:] = lowpass_filter(vec(row);
-			sampling_rate, cutoff, order, center_data)
-	end
-	return rtn
-end;
-
-
-
-
 
 function moving_average(dat::AbstractVector{<:AbstractFloat},
 	window_size::Int64)
